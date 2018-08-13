eb912ce90b27fa579f8197632b84ffb9a4bdf334
Joshua Anthony <jantho1990@gmail.com>
2018-08-13 12:29:34 -0500

    added something to initially render in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import pop from '../pop'
-const { Game, entity, math } = pop
+const { Game, entity, math, Text } = pop
 
 const game = new Game(640, 480)
 const { scene, w, h, } = game
+
+const style = {
+  fill: 'hsl(320, 20%, 20%)',
+  font: '40pt "Arial", monospace'
+}
+
+const demoText1 = new Text('Gamedev', style)
+demoText1.pos = { x: w / 4, y: 125 }
+
+const demoText2 = new Text('Boilerplate', style)
+demoText2.pos = { x: w / 3, y: 175 }
+
+scene.add(demoText1)
+scene.add(demoText2)
 
 game.run()
